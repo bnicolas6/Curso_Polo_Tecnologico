3cbd6e1385a1862830e7e7748ab6f81cde964d7f
Juan Lopez <guillekallap@hotmail.com>
2018-03-02 12:12:29 -0300

    Add files via upload

--- Clase1Def/Curso/Curso.Negocio/User.cs
+++ Clase1Def/Curso/Curso.Negocio/User.cs
@@ -3,92 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Curso.Negocio;
 
 namespace Curso.Negocio
 {
-
-    public class Persona {
-        private string name;
-        private string surname;
-        private int age;
-        private int dni;
-
-        public string Name
+        public class User : Persona
         {
-            get { return name; }
-            set { name = value; }
-        }
-        public string Surname
-        {
-            get { return surname; }
-            set { surname = value; }
-        }
-        public int Age
-        {
-            get { return age; }
-            set { age = value; }
-        }
-        public int Dni
-        {
-            get { return dni; }
-            set { dni = value; }
-        }
+            private string username;
+            private string password;
 
-        public Persona(string name, string surname, int age, int dni) {
-            Name = name;
-            Surname = surname;
-            Age = age;
-            Dni = dni;
-        }
-        
-    
-    
-    
-    }
-    public class User:Persona{
-
-        private string username;
-        private string password;
-
-        public string Username{
-            get { return username; }
-            set { username = value; }
-        }
-        public string Password{
-            get { return password; }
-            set { password = value; }
-        }
-
-        public User(string username, string password) : base(null,null,0,0){
-            Username = username;
-            Password = password;
-        }
-
-        public string toString() {
-            return "Username: " + username + " Password: " + password;
-        }
-
-        public bool verificarLogin() {
-            return (username.Length > 4 && password.Length > 9 && verificarMayus() == true) ? true : false;
-        }
-
-        private bool verificarMayus() {
-            return (Buscar.buscarMayus(username) == true && Buscar.buscarMayus(password) == true) ? true : false;
-        }
-    }
-
-    static class Buscar {
-
-        public static bool buscarMayus(string cadena) {
-            bool valor = false;
-            int i=0;
-          
-            while (i < cadena.Length && valor == false) {
-                if (cadena[i] >= 'A' && cadena[i] <= 'Z')
-                    valor = true;
-                i++;
+            public string Username
+            {
+                get { return username; }
+                set { username = value; }
             }
-            return valor;
+            public string Password
+            {
+                get { return password; }
+                set { password = value; }
+            }
+
+            public User(string username, string password) : base(null, null, 0, 0)
+            {
+                Username = username;
+                Password = password;
+            }
+
+            public string toString()
+            {
+                return "Username: " + username + " Password: " + password;
+            }
+
+            public bool verificarLogin()
+            {
+                return (username.Length > 4 && password.Length > 9 && verificarMayus() == true) ? true : false;
+            }
+
+            private bool verificarMayus()
+            {
+                return (Buscar.buscarMayus(username) == true && Buscar.buscarMayus(password) == true) ? true : false;
+            }
+        }
+
+        static class Buscar
+        {
+
+            public static bool buscarMayus(string cadena)
+            {
+                bool valor = false;
+                int i = 0;
+
+                while (i < cadena.Length && valor == false)
+                {
+                    if (cadena[i] >= 'A' && cadena[i] <= 'Z')
+                        valor = true;
+                    i++;
+                }
+                return valor;
+            }
         }
     }
-}
